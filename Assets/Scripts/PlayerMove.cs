6d095e3ad8312b09c6b9a85ede037440c4a22278
kido <61651321+syskido@users.noreply.github.com>
2025-12-20 14:30:36 +0900

    TileMap & Camara 셋팅

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -7,20 +7,25 @@ public class PlayerMove : MonoBehaviour
     Rigidbody2D rigid;
     SpriteRenderer spriteRenderer;
     Animator animator;
+    
+
 
     private void Awake()
     {
         rigid = GetComponent<Rigidbody2D>();
         spriteRenderer = GetComponent<SpriteRenderer>();
         animator = GetComponent<Animator>();
+
     }
 
     private void Update()
     {
         //Jump
-        if (Input.GetButtonDown("Jump"))
+        if (Input.GetButtonDown("Jump") && !animator.GetBool("isJumping")) {
             rigid.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
             animator.SetBool("isJumping", true);
+        }
+            
 
         //Stop Speed
         if (Input.GetButtonUp("Horizontal")) {            
@@ -40,14 +45,25 @@ public class PlayerMove : MonoBehaviour
 
     private void FixedUpdate()
     {
-        //Move BY Control
+        //Move Speed
         float h = Input.GetAxisRaw("Horizontal");
         rigid.AddForce(Vector2.right * h, ForceMode2D.Impulse);
 
+        //Max Speed
         if (rigid.velocity.x > maxSpeed) //Right Max Speed
             rigid.velocity = new Vector2(maxSpeed, rigid.velocity.y);
         else if (rigid.velocity.x < maxSpeed*(-1)) //Left Max Speed
             rigid.velocity = new Vector2(maxSpeed*(-1), rigid.velocity.y);
-            
+
+        //Landing Platform
+        if(rigid.velocity.y < 0) {
+            Debug.DrawRay(rigid.position, Vector3.down, new Color(0, 1, 0));
+            RaycastHit2D rayHit = Physics2D.Raycast(rigid.position, Vector3.down, 1, LayerMask.GetMask("Platform"));
+            if (rayHit.collider != null)
+            {
+                if (rayHit.distance < 0.5f)
+                    animator.SetBool("isJumping", false);
+            }
+        }
     }
 }
